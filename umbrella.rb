4a8d24fda4bf1749181cad22f37458c9c9f6cdca
klee2024 <klee2024@uchicago.edu>
2023-01-20 01:41:54 +0000

    mvp umbrella weather forecaster

--- umbrella.rb
+++ umbrella.rb
@@ -1,8 +1,11 @@
+p "====================================="
+p "Will you need an umbrella today?"
+p "====================================="
 p "Where are you located?"
 
-#user_location = gets.chomp
+user_location = gets.chomp
 
-user_location = "Chicago"
+p "Checking the weather at #{user_location}..."
 
 # google maps url 
 # what can i modify about the url to get different info
@@ -26,9 +29,62 @@ location = geometry.fetch("location")
 lat = location.fetch("lat")
 long = location.fetch("lng")
 
+p "Your coordinates are #{lat}, #{long}."
+
+
 # there's a method called .dig that is probably faster
 
-p lat 
-p long
-
 # print driven development to make sure that each step it's accomplishing what you think
+
+dark_sky_url = "https://api.darksky.net/forecast/#{ENV.fetch("DARK_SKY_KEY")}/#{lat},#{long}"
+
+#p dark_sky_url
+
+raw_dark_sky_data = URI.open(dark_sky_url).read
+parsed_dark_sky_data =  JSON.parse(raw_dark_sky_data)
+
+#p parsed_dark_sky_data
+
+# Display the current temperature and summary of the weather for the next hour.
+
+dark_sky_results = parsed_dark_sky_data
+
+#p dark_sky_results
+
+dark_sky_results_currently = dark_sky_results.fetch("currently")
+
+curr_temp = dark_sky_results_currently.fetch("temperature")
+curr_temp_str = "it is currently #{curr_temp} degrees"
+
+p curr_temp_str
+
+dark_sky_results_hourly = dark_sky_results.fetch("hourly")
+
+hourly_summary = dark_sky_results_currently.fetch("summary")
+hourly_str = "Next hour: #{hourly_summary}"
+
+p hourly_str
+
+hourly_data = dark_sky_results_hourly.fetch("data")
+
+# next 12 hours not including the current hour
+next_12_hours = hourly_data[1, 13]
+
+#p hourly_data[0]
+
+count = 1
+rain_likely = false
+next_12_hours.each do |one_hour|
+  precipitation = one_hour.fetch("precipProbability")
+  if precipitation > 0.1
+   p "There is a #{precipitation} probability of rain #{count} hours from now!"
+   rain_likely = true
+  end
+  count += 1
+end
+
+if rain_likely
+  p "You might want to carry an umbrella!"
+else
+  p "You probably won't need an umbrella today"
+end
